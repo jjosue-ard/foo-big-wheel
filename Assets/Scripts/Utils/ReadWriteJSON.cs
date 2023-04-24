b1dfeef5e4258eb5c717680b5384def2fb4e4c30
ARISTOCRAT\JJ101331 <jehoshua.josue@aristocrat.com>
2023-04-24 12:05:18 -0700

    Fixed ReadWriteJSON

--- Assets/Scripts/Utils/ReadWriteJSON.cs
+++ Assets/Scripts/Utils/ReadWriteJSON.cs
@@ -16,10 +16,10 @@ namespace Assets.Scripts.Utils
     public class ReadWriteJSON
     {
 
-        public static ReelStripDataModel ReadRecordsForPocketScenario(string fileName)
+        public static ReelStripDataModel ReadReelDataFromFile(string fileName)
         {
             ReelStripDataModel result;            
-            string filePath = Path.Combine(Application.streamingAssetsPath, "Recorded_Scenarios", fileName);
+            string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
             string fileContent = ReadWriteFileScript.EnsureReadFile(filePath);
 
             result = JsonConvert.DeserializeObject<ReelStripDataModel>(fileContent);
